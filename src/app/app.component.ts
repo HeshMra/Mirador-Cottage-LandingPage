613238e6d7ec6f6127b7692eb5d6bd8fbfb6362d
heshith <heshith619@gmail.com>
2024-11-20 21:03:20 +0530

    about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../Components/nav-bar/nav-bar.component';
 import { HeroSectionComponent } from '../Components/hero-section/hero-section.component';
+import { AboutSectionComponent } from '../Components/about-section/about-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent,HeroSectionComponent],
+  imports: [NavBarComponent,HeroSectionComponent,AboutSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
