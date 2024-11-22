b35567a57b4193e37c9ff3dc7921805b5a128091
heshith <heshith619@gmail.com>
2024-11-22 11:11:50 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../Components/nav-bar/nav-bar.component';
 import { HeroSectionComponent } from '../Components/hero-section/hero-section.component';
 import { AboutSectionComponent } from '../Components/about-section/about-section.component';
+import { GallerySectionComponent } from '../Components/gallery-section/gallery-section.component';
+import { ContactSectionComponent } from '../Components/contact-section/contact-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavBarComponent,HeroSectionComponent,AboutSectionComponent],
+  imports: [NavBarComponent,HeroSectionComponent,AboutSectionComponent,GallerySectionComponent,ContactSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
